0a3747748bfa4399b11c0925c3a44d7e440ed50f
Leandro <leandro.silva343@fatec.sp.gov.br>
2024-10-05 11:01:44 -0300

    feat(p1-pdmt): cria componente principal e faz hello

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col className="text-center">
+          <h1>Hello, lembretes</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
